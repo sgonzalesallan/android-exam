803e7858df3337091c28d3aa361bfb8b8395109f
rygalang <ronaldgalangy@gmail.com>
2017-12-28 19:27:34 +0800

    Added appconstant.

---
+++ app/src/main/java/com/rygalang/androidexam/base/AppConstant.java
@@ -0,0 +1,9 @@
+package com.rygalang.androidexam.base;
+
+/**
+ * Created by Computer3 on 12/28/2017.
+ */
+
+public class AppConstant {
+    public static final String HOST_URL = "https://raw.githubusercontent.com/rygalang/android-exam/develop/rest_api/";
+}
