aff3a23179b0b73488a73849a9b8b08a1ee65766
rygalang <ronaldgalangy@gmail.com>
2017-12-28 19:37:29 +0800

    An interface for person rest api endpoint.

---
+++ app/src/main/java/com/rygalang/androidexam/person/service/PersonService.java
@@ -0,0 +1,18 @@
+package com.rygalang.androidexam.person.service;
+
+import com.rygalang.androidexam.model.Person;
+
+import java.util.ArrayList;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+
+/**
+ * Created by Computer3 on 12/28/2017.
+ */
+
+public interface PersonService {
+
+    @GET("person")
+    Single<ArrayList<Person>> getPersonList();
+}
